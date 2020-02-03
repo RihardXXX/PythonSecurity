a678b91fd12b968d0f0d4950a7fbb569a4f2c588
RihardXXX <yusupa.akaeva@yandex.ru>
2020-02-03 17:42:44 -0500

    bag in ip address(((

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env/ python
 
 import scapy.all as scapy
-from scapy.all import *
+
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)                           # Create object ARP request
-    print(arp_request.summary())                  # <-------- not worjing program
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")           # Create package send
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request              # sum two pakcage
+    arp_request_broadcast.show()
 
 
 
-scan("10.0.2.1/24")
+
+scan("10.0.2.1")
